b2280ebf4cb50df6fd1f999059955b5e94295e94
gautam678 <gautam678@gmail.com>
2017-12-10 15:33:00 -0500

    :fire: Rollback chartjs stuff

--- supervisor-client/src/app/app.component.ts
+++ supervisor-client/src/app/app.component.ts
@@ -1,29 +1,10 @@
 import { Component } from '@angular/core';
-import { get_data } from './test';
-let values = get_data('age');
-let labels = values[0];
-let list_v1 = values[1];
-let list_v2 = values[2];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-
 export class AppComponent {
   title = 'app';
-  chartOptions = {
-    responsive: true
-  };
-chartType = 'bar';
-  chartData = [
-    { data: list_v1, label: '>50K' },
-    {data: list_v2, label: '<=50k' }
-  ];
-
-  chartLabels = labels;
-
-  onChartClick(event) {
-    console.log(event);
-  }
 }
